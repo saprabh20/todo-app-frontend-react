32c294678210cd6c8887dbdd6a9245fece4c7768
saprabh20 <saprabhanand20@gmail.com>
2024-05-06 11:45:48 +0530

    todos change

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,17 +1,20 @@
+import { useRef } from "react";
 import Todo from "./Todo";
 
 const Todos = ({ todos, deleteTodo }) => {
+    
+    const myRef = useRef(null);
+    const executeScroll = () => myRef.current.scrollIntoView();
     return (
         <>
             {todos.map((todo) => {
                 return (
-                    <div
-                        key={todo.id}
-                    >
-                        <Todo todo={todo} deleteTodo={deleteTodo}/>
+                    <div key={todo.id}>
+                        <Todo todo={todo} deleteTodo={deleteTodo} executeScroll={executeScroll}/>
                     </div>
                 );
             })}
+            <div ref={myRef}></div>
         </>
     );
 };
